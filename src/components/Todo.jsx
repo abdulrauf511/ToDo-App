f2ff643081025a29c5b780f109683373f280766c
Abdul Rauf <abdulraufcs23@gmail.com>
2024-11-07 20:50:49 +0500

    implement todo list functionality with add, delete, and toggle features; update TodoItems component for dynamic rendering

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,48 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon.png";
 import TodoItems from "./TodoItems";
 
 const ToDo = () => {
   const [todoList, setTodoList] = useState([]);
+
   const inputRef = useRef();
+
   const add = () => {
     const inputText = inputRef.current.value.trim();
-    console.log(inputText);
+
+    if (inputText === "") {
+      return null;
+    }
+
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
   };
+
+  const deleteTodo = (id) => {
+    setTodoList((prvTodos) => {
+      return prvTodos.filter((todo) => todo.id !== id);
+    });
+  };
+
+  const toggle = (id) => {
+    setTodoList((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isComplete: !todo.isComplete };
+        }
+        return todo;
+      });
+    });
+  };
+
+  useEffect(() => {
+    console.log(todoList);
+  }, [todoList]);
 
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
@@ -36,8 +70,18 @@ const ToDo = () => {
 
       {/* ------------ todo list ------------ */}
       <div>
-        <TodoItems text="Learn Coding" />
-        <TodoItems text="Learn Coding from AbdulRauf" />
+        {todoList.map((item, index) => {
+          return (
+            <TodoItems
+              key={index}
+              text={item.text}
+              id={item.id}
+              isComplete={item.isComplete}
+              deleteTodo={deleteTodo}
+              toggle={toggle}
+            />
+          );
+        })}
       </div>
     </div>
   );
