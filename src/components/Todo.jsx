2e2f224b0abfbea47bf4d0d0764dc7678bdfdf42
Abdul Rauf <abdulraufcs23@gmail.com>
2024-11-07 20:20:37 +0500

    add TodoItems component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon.png";
+import TodoItems from "./TodoItems";
 
 const ToDo = () => {
+  const [todoList, setTodoList] = useState([]);
+  const inputRef = useRef();
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+    console.log(inputText);
+  };
+
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
       {/* ------------ title ------------ */}
@@ -13,17 +21,24 @@ const ToDo = () => {
       {/* ------------ input ------------ */}
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
         <input
+          ref={inputRef}
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
           type="text"
           placeholder="Add your task"
         />
-        <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">
+        <button
+          onClick={add}
+          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+        >
           ADD +
         </button>
       </div>
 
       {/* ------------ todo list ------------ */}
-      <div></div>
+      <div>
+        <TodoItems text="Learn Coding" />
+        <TodoItems text="Learn Coding from AbdulRauf" />
+      </div>
     </div>
   );
 };
